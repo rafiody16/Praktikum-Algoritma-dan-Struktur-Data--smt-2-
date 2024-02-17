24ae1461d12c4be308a0e884063426a7b4214ba4
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-02-17 21:33:29 +0700

    function

--- Jobsheet 1/function.java
+++ Jobsheet 1/function.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class function {
 
+    private static Scanner sc = new Scanner(System.in);
     private static String toko [] = {"Royal Garden 1", "Royal Garden 2", "Royal Garden 3", "Royal Garden 4"};
     private static String bunga[] = {"Aglonema", "Keladi", "Alocasia", "Mawar"};
     private static int stok [][] = {
@@ -11,60 +12,88 @@ public class function {
         { 5, 7, 12, 9 }
     };
     private static int harga [] = {75000, 50000, 60000, 10000};
-    private static int pengurangan [] = {1, 2, 0, 5};
+    private static int[][] bgMati = new int[4][4];
+    private static int[][] terjual = new int[4][4];
+
+    public static void stokBunga() {
+        System.out.print("Silahkan Pilih Cabang(1-4): ");
+        int cabToko = sc.nextInt();
+        System.out.println("Stok Bunga Cabang "+ toko[cabToko - 1]);
+        System.out.println("=====================================================================");
+        System.out.printf("%-15s | %-15s | %-15s | %-15s\n", "Bunga", "Stok", "Terjual", "Bunga Mati");
+        System.out.println("=====================================================================");
+        for (int i = 0; i < bunga.length; i++) {
+            System.out.printf("%-15s | %-15s | %-15s | %-15s\n", bunga[i], stok[cabToko - 1][i], terjual[cabToko - 1][i], bgMati[cabToko - 1][i]);
+        }
+    }
+
+    public static void bungaMati(){
+        System.out.print("Silahkan Pilih Cabang(1-4): ");
+        int cabToko = sc.nextInt();
+        System.out.println("Data Bunga Mati Pada Cabang "+ toko[cabToko - 1]);
+        for (int i = 0; i < terjual.length; i++) {
+            System.out.print("Jumlah Bunga "+ bunga[i] +"yang mati: ");
+            bgMati[cabToko - 1][i] = sc.nextInt();
+            stok[cabToko - 1][i] -= bgMati[cabToko - 1][i];
+        }
+        System.out.println("===========================================");
+        System.out.printf("%-15s | %-15s | %-15s\n", "Bunga", "Jumlah Mati", "Sisa Bunga");
+        System.out.println("===========================================");
+        for (int x = 0; x < bunga.length; x++) {  
+            System.out.printf("%-15s | %-15s | %-15s\n", bunga[x], bgMati[cabToko - 1][x], stok[cabToko - 1][x]);          
+        }
+    }
 
     public static void pendapatanCabang() {
-        for (int i = 0; i < stok.length; i++) {
-            int pendapatan = 0;
-            for (int j = 0; j < stok[i].length; j++) {
-                int hrgbunga = 0;
-                switch (j) {
-                    case 0:
-                        hrgbunga = harga[0];
-                        break;
-                    case 1:
-                        hrgbunga = harga[1];
-                        break;
-                    case 2:
-                        hrgbunga = harga[2];
-                        break;
-                    case 3:
-                        hrgbunga = harga[3];
-                        break;
-                    default:
-                        break;
-                }
-                pendapatan += stok[i][j] * hrgbunga;
-            }
-            System.out.println("Cabang "+ toko[i] +": "+ pendapatan);
+        System.out.print("Silahkan Pilih Cabang(1-4): ");
+        int plhCbg = sc.nextInt();
+        for (int i = 0; i < terjual.length; i++) {
+            System.out.print("Jumlah Bunga "+ bunga[i] +"yang terjual: ");
+            terjual[plhCbg - 1][i] = sc.nextInt();
+            stok[plhCbg - 1][i] -= terjual[plhCbg - 1][i];
         }
-    }
-
-    public static void stokBunga(int cabToko) {
-        System.out.println("Stok royal garden "+cabToko+": ");
-        for (int i = 0; i < stok[cabToko - 1].length; i++) {
-            System.out.println(stok[cabToko - 1][i]);
-        }
-    }
-
-    public static void updateStok(){
-        for (int i = 0; i < stok.length; i++) {
-            for (int j = 0; j < stok[i].length; j++) {
-                stok[i][j] -= pengurangan[j];
-            }            
+        System.out.println("Pendapatan Cabang "+ toko[plhCbg - 1]);
+        System.out.println("===========================================");
+        System.out.printf("%-15s | %-15s | %-15s\n", "Bunga", "Terjual", "Omset");
+        System.out.println("===========================================");
+        int pendapatan = 0;
+        for (int x = 0; x < harga.length; x++) {
+            pendapatan += harga[x] * terjual[plhCbg - 1][x];
+            System.out.printf("%-15s | %-15s | %-15s\n", bunga[x], terjual[plhCbg - 1][x], pendapatan);
         }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] terjual = new int[4];
-        for (int i = 0; i < 4; i++) {
-            System.out.print("Jumlah Bunga "+ bunga[i] +"yang terjual: ");
-            terjual[i] = sc.nextInt();
-        }
-        pendapatanCabang();
-        stokBunga(4);
-        updateStok();
+        System.out.println("Toko Bunga Royalgarden");
+        System.out.println("===========================");
+        boolean exit = false;
+        do {
+            System.out.println("1. Pendapatan\n2. Bunga Mati\n3. Stock Bunga\n0. Exit");
+            System.out.println("Pilih menu: ");
+            int select = sc.nextInt();
+            switch (select) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    pendapatanCabang();
+                    break;
+                case 2:
+                    bungaMati();
+                    break;
+                case 3:
+                    stokBunga();
+                    break;
+                default:
+                    break;
+            }
+            System.out.println("Kembali ke menu utama? (1 = Ya, 0 = Tidak): ");
+            int backToMenu = sc.nextInt();
+            if (backToMenu != 1) {
+                exit = true;
+            }
+        } while (!exit);
     }
-    
+        
+
 }
