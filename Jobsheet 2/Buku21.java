376151f53efc99fc5d3eb3e6ee7b679e8591dc30
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-02-22 23:22:51 +0700

    Jobsheet 2

---
+++ Jobsheet 2/Buku21.java
@@ -0,0 +1,35 @@
+public class Buku21 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi () {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: " + harga);        
+    }
+
+    void terjual (int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("Stok sudah habis!");
+        }
+    }
+
+    void restock (int jml) {
+        stok += jml;
+    }
+    
+    void gantiHarga (int hrg) {
+        harga = hrg;
+    }
+
+}
+
+// public Buku21(String jud, String pg, int hal, int stok, int har) {
+//     judul = jud;
+//     pengarang = pg;
+// }
---
+++ Jobsheet 2/Buku21Main.java
@@ -0,0 +1,18 @@
+
+// public class Buku21Main {
+    
+//     public static void main(String[] args) {
+//         Buku21 bk1 = new Buku21();
+//         bk1.judul = "Today Ends Tomorrow Comes";
+//         bk1.pengarang = "Denanda Pratiwi";
+//         bk1.halaman = 198;
+//         bk1.stok = 13;
+//         bk1.harga = 71000;
+
+//         bk1.tampilInformasi();
+//         bk1.terjual(5);
+//         bk1.gantiHarga(60000);
+//         bk1.tampilInformasi();
+//     }
+
+// }
