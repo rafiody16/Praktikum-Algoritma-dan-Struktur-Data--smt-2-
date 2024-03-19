55fa05217d2744f0344dd4517acdb8507827fee3
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-03-19 13:49:51 +0700

    Jobsheet 4

--- Jobsheet 4/BruteForceDivideConquer/Faktorial21.java
+++ Jobsheet 4/BruteForceDivideConquer/Faktorial21.java
@@ -5,10 +5,14 @@ public class Faktorial21 {
     int faktorialBF(int n)
     {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+
+        while (i <= n) {
             fakto = fakto * i;
+            i++;            
         }
         return fakto;
+        
     }
 
     int faktorialDC(int n)
