57b46d3545a151f5e285ac348f26df96b3b6fc35
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-03-19 15:53:37 +0700

    Jobsheet 4

---
+++ Jobsheet 4/BruteForceDivideConquer/Sum21.java
@@ -0,0 +1,36 @@
+public class Sum21 {
+
+    public int elemen;
+    public double keuntungan[], total;
+
+    public Sum21(int elemen)
+    {
+        this.elemen = elemen;
+        this.keuntungan = new double[elemen];
+        this.total = 0;
+    }
+
+    public double totalBF(double arr[])
+    {
+        for (int i = 0; i < elemen; i++) {
+            total = total + arr[i];
+        }
+        return total;
+    }
+
+    public double totalDC(double arr[], int l, int r)
+    {
+        if (l == r) {
+            return arr[l];
+        }
+        else if (l < r)
+        {
+            int mid = (l + r) / 2;
+            double lsum = totalDC(arr, l, mid - 1);
+            double rsum = totalDC(arr, mid + 1, r);
+            return lsum + rsum + arr[mid];
+        }
+        return 0;
+    }
+    
+}
