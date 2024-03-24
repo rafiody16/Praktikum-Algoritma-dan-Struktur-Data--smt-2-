f094acf9d140c5babf6f4fe9df43419e06cc133d
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-03-24 10:19:56 +0700

    Jobsheet 5

--- Jobsheet 5/Percobaan/DaftarMahasiswaBerprestasi21.java
+++ Jobsheet 5/Percobaan/DaftarMahasiswaBerprestasi21.java
@@ -28,7 +28,7 @@ public class DaftarMahasiswaBerprestasi21 {
     {
         for (int i = 0; i < listMhs.length-1; i++) {
             for (int j = 1; j < listMhs.length-i; j++) {
-                if (listMhs[j].ipk > listMhs[j-1].ipk) {
+                if (listMhs[j].ipk < listMhs[j-1].ipk) {
                     Mahasiswa21 tmp = listMhs[j];
                     listMhs[j] = listMhs[j-1];
                     listMhs[j-1] = tmp;
