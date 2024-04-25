8363dcf3c6869b0d68f5b717a99c86ba96526576
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-04-25 20:32:01 +0700

    Jobsheet 8

---
+++ Jobsheet 8/Percobaan 1/QueueMain21.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class QueueMain21 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas queue: ");
+        int n = sc.nextInt();
+        Queue21 Q = new Queue21(n);
+
+        int pilih;
+
+        do {
+            menu();
+            pilih = sc.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru: ");
+                    int dataMasuk = sc.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang keluar: "+ dataKeluar);
+                        break;
+                    }
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+                default:
+                    break;
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+
+        sc.close();
+    }
+
+    public static void menu() {
+        System.out.println("Masukkan operasi yang diinginkan: ");
+        System.out.println("1. Enqueue\n2. Dequeue\n3. Print\n4. Peek\n5. Clear");
+        System.out.println("------------------------------");
+    }
+    
+}
