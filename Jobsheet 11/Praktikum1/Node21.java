f62c77c36f51ddffd69c1b763f7f73b29cdd06ec
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-05-31 09:18:55 +0700

    Praktikum 1

---
+++ Jobsheet 11/Praktikum1/Node21.java
@@ -0,0 +1,16 @@
+public class Node21 {
+
+    int data;
+    Node21 left, right;
+
+    public Node21() {
+
+    }
+
+    public Node21(int data) {
+        this.left = left;
+        this.data = data;
+        this.right = right;
+    }
+    
+}
