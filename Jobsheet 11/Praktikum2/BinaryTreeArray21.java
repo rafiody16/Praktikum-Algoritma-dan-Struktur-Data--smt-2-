1a7f2d7ec7eb74a4fd2155d05fea7c43b1c85d43
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-05-31 13:01:14 +0700

    Praktikum 2

---
+++ Jobsheet 11/Praktikum2/BinaryTreeArray21.java
@@ -0,0 +1,23 @@
+public class BinaryTreeArray21 {
+
+    int[] data;
+    int idxLast;
+
+    public BinaryTreeArray21() {
+        data = new int[10];
+    }
+
+    public void populateData(int data[], int idxLast){
+        this.data = data;
+        this.idxLast = idxLast;
+    }
+
+    public void traverseInOrder(int idxStart){
+        if (idxStart <= idxLast) {
+            traverseInOrder(2*idxStart+1);
+            System.out.print(data[idxStart] + " ");
+            traverseInOrder(2*idxStart+2);
+        }
+    }
+    
+}
